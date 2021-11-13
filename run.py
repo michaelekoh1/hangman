731468d5790bb28be8df1923c9287b76f1d42179
michaelekoh1 <michael.ekoh@hotmail.com>
2021-11-13 00:47:37 +0000

    Added the function to get a random word from the list

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import string
+from words import words
+
+def game_starts():
+    """The initial stage of the game where a user's name is requested and if he/she would like to commence with the game.
+    """
+    print(
+        """
+        ██   ██  █████  ███    ██  ██████  ███    ███  █████  ███    ██ 
+        ██   ██ ██   ██ ████   ██ ██       ████  ████ ██   ██ ████   ██ 
+        ███████ ███████ ██ ██  ██ ██   ███ ██ ████ ██ ███████ ██ ██  ██ 
+        ██   ██ ██   ██ ██  ██ ██ ██    ██ ██  ██  ██ ██   ██ ██  ██ ██ 
+        ██   ██ ██   ██ ██   ████  ██████  ██      ██ ██   ██ ██   ████ 
+                                                                                                                                                                                                       
+        """
+    )
+
+    name = input('What is your name?\n')
+    print(f'Hello, {name}')
+    if input('Would you like to play the Hangman game? (Y)').upper() == "Y":
+        hangman()
+
+    else:
+        print('Try Again')
+        game_starts()
+
+# function to get a random word from the list
+def get_random_word():
+    """
+    A random word is picked from the list of words for the player to guess
+    """
+    word = random.choice(words)
+    return word.upper()
+
+def hangman():
+    """Play the game"""
+    word = get_random_word()
+    word_letters = set(word) 
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # letters the user has guessed
+
+    lives = 7
